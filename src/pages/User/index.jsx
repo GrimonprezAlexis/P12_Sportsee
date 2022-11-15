112fb503c5f7f3460d2893ce873300e15088d42d
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2022-11-15 01:08:58 +0100

    HTTP GET getUserById and show in UserNameBanner

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import UserNameBanner from "../../components/User";
+import { getUserById } from '../../services/userService'
 
 const User = () => {
-  const {id} = useParams();
+  
+  // Prepare request service to getUserById
+  const [data, setData] = useState([]);
+	const {id} = useParams();
+
+  useEffect(() => {
+		const getData = async () => {
+			const request = await getUserById(id);
+			if (!request) return alert('data error');
+			setData(request.data);
+		};
+		getData();
+	}, [id]);
+	if (data.length === 0) return null;
+
+
   return (
     <div>
       <h1>User {id}👩‍💻👨‍💻👩‍💻</h1>
+      <UserNameBanner name={data.userInfos.firstName}/>
+      
     </div>
   )
 }
---
+++ src/services/userService.jsx
@@ -0,0 +1,60 @@
+import axios from 'axios';
+const apiUrl = process.env.REACT_APP_API_URL
+
+
+/**
+ * Get user infos
+ * @param {string} id 
+ * @returns {object}
+ */
+export const getUserById = async (id) => {
+	try {
+		const res = await axios.get(`${apiUrl}/user/${id}`);
+		return res.data;
+	} catch (e) {
+		console.log(e);
+	}
+};
+
+
+/**
+ * Get user activity 
+ * @param {string}
+ * @returns {object}
+ */
+export const getUserActivity = async (id) => {
+	try {
+		const res = await axios.get(`${apiUrl}/user/${id}/activity`);
+		return res.data;
+	} catch (e) {
+		console.log(e);
+	}
+};
+
+/**
+ * Get user average session 
+ * @param {string}
+ * @returns {object}
+ */
+export const getUserAverageSessions = async (id) => {
+	try {
+		const res = await axios.get(`${apiUrl}/user/${id}/average-sessions`);
+		return res.data;
+	} catch (e) {
+		console.log(e);
+	}
+};
+
+/**
+ * Get user performance 
+ * @param {string}
+ * @returns {object}
+ */
+export const getUserPerformance = async (id) => {
+	try {
+		const res = await axios.get(`${apiUrl}/user/${id}/performance`);
+		return res.data;
+	} catch (e) {
+		console.log(e);
+	}
+};
